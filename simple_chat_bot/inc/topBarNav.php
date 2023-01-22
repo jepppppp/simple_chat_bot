b8f62a265eacae2b9f62f0146111f9cabcd45b9a
jepppppp <jeffersonsornio@gmail.com>
2023-01-21 23:01:44 -0700

    chatbot activity

---
+++ simple_chat_bot/inc/topBarNav.php
@@ -0,0 +1,21 @@
+<script>
+  $(function(){
+    $('#login-btn').click(function(){
+      uni_modal("","login.php")
+    })
+    $('#navbarResponsive').on('show.bs.collapse', function () {
+        $('#mainNav').addClass('navbar-shrink')
+    })
+    $('#navbarResponsive').on('hidden.bs.collapse', function () {
+        if($('body').offset.top == 0)
+          $('#mainNav').removeClass('navbar-shrink')
+    })
+  })
+
+  $('#search-form').submit(function(e){
+    e.preventDefault()
+     var sTxt = $('[name="search"]').val()
+     if(sTxt != '')
+      location.href = './?p=products&search='+sTxt;
+  })
+</script>
